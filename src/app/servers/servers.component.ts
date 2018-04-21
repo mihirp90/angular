18d652185e92c8c5f2afca7e2be3b1c87c095de7
mihirp90 <mihirp90@gmail.com>
2018-04-21 16:51:11 +0530

    update message

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  serveCreationStatus = 'No server was created!';
+  serveCreationStatus = 'No server was created!, please create server';
   serverName = 'TestServer';
   serverCreated = false;
   servers =['TestServer', 'TestServer2'];
