8fbc078ccafcf3e0d7c5723d7c3a90684ef105b8
mihirp90 <mihirp90@gmail.com>
2018-04-21 14:48:10 +0530

    first commit of angular 5 project

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  //selector: '[app-servers]',
+  //selector: '.app-servers',
+  selector:'app-servers',
+  templateUrl:'./servers.component.html',
+  // template: `<app-server></app-server>
+  //           <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serveCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers =['TestServer', 'TestServer2'];
+  constructor() {
+      setTimeout(() =>{
+        this.allowNewServer = true;
+      }, 2000);
+   }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serveCreationStatus ='Server was created and name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
